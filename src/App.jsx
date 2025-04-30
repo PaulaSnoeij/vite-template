cc23b4415766655040e857deea039fa68b485d74
Paula Snoeij <snoeijpaula@gmail.com>
2025-04-30 17:32:53 +0200

    Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
 import { DrinkButtons } from './components/DrinkButtons';
-import {tea, coffee } from './utils/data';
+import { DrinkChoice } from './components/DrinkChoice';
+import {coffee, tea } from './utils/data';
+
 
 export const App = () => {
 	const greeting = 'Welcome to our cafe!';
+
+	const userDring = 'tea'; // This will be replaced with the user's choice
 
 	return (
 		<div className="app">
 			<h1>{greeting}</h1>
 			<DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
+			<DrinkChoice drink={userDrink} />
 		</div>
 	);
 };
+
---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,7 @@
+import './Buttons.css';
+
+function Button({ text }) {
+    return <button className="Button">{text}</button>;
+};
+
+export default Button;
